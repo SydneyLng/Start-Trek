f9d5340541d547b5e1251771eb72b61a28d4b45f
Sydney Luong <sydney.luong@epitech.eu>
2026-04-14 16:13:43 +0200

    feat(cli): add train, eval and reproduce subcommands
    Set up argparse subparsers wih required --config and optional --run_id to standardize experiments command entrypoints

--- src/main.py
+++ src/main.py
@@ -1,14 +1,51 @@
-import gymnasium as gym
+import argparse
+import textwrap
 
-env = gym.make("LunarLander-v3")
+parser = argparse.ArgumentParser(
+    prog='Start trek',
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    description=textwrap.dedent('''\
+            Start trak is a reinforcement learning project
+            that enables modules to correct its own trajectory
+            to land safely on the moon"
+        '''))
 
-obs, info = env.reset()
+subparsers = parser.add_subparsers(dest="command", required=True)
 
-while True:
-    action = env.action_space.sample()
-    obs, reward, terminated, truncated, info = env.step(action)
-    if terminated or truncated:
-        obs, info = env.reset()
-    env.render()
+#train
+train_parser = subparsers.add_parser(
+    "train",
+    help="Run the training loop (data collection + optimization) and save metrics/checkpoints for the configured run",
+)
+train_parser.add_argument(
+    "--config",
+    type=str,
+    required=True,
+    help="Path to YAML config file",
+)
+train_parser.add_argument(
+    "--run_id",
+    type=str,
+    help="Experiment run Id (output folder name)",
+)
 
-env.close()
+#eval
+
+eval_parser = subparsers.add_parser(
+    "eval",
+    help="Evaluate a trained agent and export metrics",
+)
+eval_parser.add_argument("--config", type=str, required=True)
+eval_parser.add_argument("--run_id", type=str)
+
+#reproduce
+repro_parser = subparsers.add_parser(
+    "reproduce",
+    help="Re-run experiements to regenerate outputs",
+)
+repro_parser.add_argument("--config", type=str, required=True)
+repro_parser.add_argument("--run_id", type=str)
+
+args = parser.parse_args()
+
+print(args.config)
